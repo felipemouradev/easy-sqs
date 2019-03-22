5ec01a717d0b49d7a0e8f8472d16fc9748dd4d62
felipemoura <felipemouracv@gmail.com>
2019-03-21 23:33:11 -0300

    FIX: Refact sendMessage now is possible send params for queue fifo and fixes error

--- lib/EasySqs.ts
+++ lib/EasySqs.ts
@@ -6,125 +6,131 @@ import interfaces = require("./Interfaces");
 
 export class Queue implements interfaces.IQueue {
 
-  public queueName: string;
-  private sqs: AWS.SQS;
+    public queueName: string;
+    private sqs: AWS.SQS;
 
-  constructor(queueName: string, sqs: AWS.SQS) {
+    constructor(queueName: string, sqs: AWS.SQS) {
 
-    if (queueName == null) throw new errors.NullOrEmptyArgumentError("queueName");
-    if (queueName.length == 0) throw new errors.InvalidArgumentError("queueName not provided");
-    if (sqs == null) throw new errors.NullOrEmptyArgumentError("sqs");
+        if (queueName == null) throw new errors.NullOrEmptyArgumentError("queueName");
+        if (queueName.length == 0) throw new errors.InvalidArgumentError("queueName not provided");
+        if (sqs == null) throw new errors.NullOrEmptyArgumentError("sqs");
 
-    this.queueName = queueName;
-    this.sqs = sqs;
+        this.queueName = queueName;
+        this.sqs = sqs;
 
-  }
-
-  //public createBatchDeleter(batchSize?: number) {
-  //  return new md.MessageDeleter(this.sqs, this.queueName, batchSize, null);
-  //}
-
-  public createQueueReader(batchSize?: number): interfaces.IQueueReader {
-    return new reader.QueueReader(this.sqs, this.queueName, batchSize);
-  }
-
-  public createMessageStream(highWaterMark?: number, batchSize?: number): interfaces.IMessageStream {
-    var rdr = new reader.QueueReader(this.sqs, this.queueName, batchSize);
-
-    var opts: interfaces.IMessageStreamOptions = null;
-
-    if (highWaterMark != null) {
-      opts = {
-        highWaterMark: highWaterMark
-      };
     }
 
-    return new stream.MessageStream(rdr, opts);
-  }
+    //public createBatchDeleter(batchSize?: number) {
+    //  return new md.MessageDeleter(this.sqs, this.queueName, batchSize, null);
+    //}
 
-  public drain(callback?: (err: Error) => void) {
-
-    var queue = this;
-
-    var queueReader = queue.createQueueReader();
-
-    queueReader
-      .on("message", (msg, context) => {
-
-        context.deleteMessage(msg);
-
-      });
-
-    queueReader
-      .on("empty", (err) => {
-      //all done, stop monitoring the queue
-      queueReader.stop();
-
-      if (callback != null) {
-        callback(err);
-      }
-
-    });
-
-    queueReader.start();
-  }
-
-  getMessage(callback: (err: Error, data: AWS.Sqs.Message) => void) {
-
-    var client = this.sqs;
-    var params: AWS.Sqs.ReceiveMessageRequest = {};
-
-    params.QueueUrl = this.queueName,
-    params.MaxNumberOfMessages = 1;
-
-    client.receiveMessage(params, function (err: Error, data: AWS.Sqs.ReceiveMessageResult) {
-
-      if (data.Messages != null && data.Messages.length > 0) {
-
-        var msg = data.Messages[0];
-        callback(err, msg);
-
-      }
-    });
-
-  }
-
-  deleteMessage(msg: AWS.Sqs.Message, callback: (err: Error) => void) {
-    var client = this.sqs;
-    var params: AWS.Sqs.DeleteMessageRequest = {};
-
-    if (msg == null) {
-      callback(new errors.NullOrEmptyArgumentError("msg"));
-      return;
+    public createQueueReader(batchSize?: number): interfaces.IQueueReader {
+        return new reader.QueueReader(this.sqs, this.queueName, batchSize);
     }
 
-    if (msg.ReceiptHandle == null || msg.ReceiptHandle.length == 0) {
-      callback(new errors.InvalidArgumentError("msg.ReceiptHandle cannot be null or empty")); return;
+    public createMessageStream(highWaterMark?: number, batchSize?: number): interfaces.IMessageStream {
+        let rdr = new reader.QueueReader(this.sqs, this.queueName, batchSize);
+
+        let opts: interfaces.IMessageStreamOptions = null;
+
+        if (highWaterMark != null) {
+            opts = {
+                highWaterMark: highWaterMark
+            };
+        }
+
+        return new stream.MessageStream(rdr, opts);
     }
 
-    params.QueueUrl = this.queueName,
-    params.ReceiptHandle = msg.ReceiptHandle;
+    public drain(callback?: (err: Error) => void) {
 
-    client.deleteMessage(params, function (err: Error, data: any) {
-      callback(err);
-    });
-  }
+        let queue = this;
 
-  sendMessage(data: string, callback: (err: Error) => void) {
-    var client = this.sqs;
+        let queueReader: any = queue.createQueueReader();
 
-    if (data == null) { callback(new errors.NullOrEmptyArgumentError("Data cannot be null")); return; }
-    if (data.length > 262144) { callback(new errors.InvalidArgumentError("data too large for SQS")); return; }
+        queueReader
+            .on("message", (msg, context) => {
 
-    var params: AWS.Sqs.SendMessageRequest = {};
+                context.deleteMessage(msg);
 
-    params.QueueUrl = this.queueName;
-    params.MessageBody = data;
+            });
 
-    client.sendMessage(params, function (err, data) {
+        queueReader
+            .on("empty", (err) => {
+                //all done, stop monitoring the queue
+                queueReader.stop();
 
-      callback(err);
-    });
+                if (callback != null) {
+                    callback(err);
+                }
 
-  }
+            });
+
+        queueReader.start();
+    }
+
+    getMessage(callback: (err: Error, data: AWS.SQS.Message) => void) {
+
+        let client = this.sqs;
+        let params: any | AWS.SQS.ReceiveMessageRequest = {};
+
+        params.QueueUrl = this.queueName,
+            params.MaxNumberOfMessages = 1;
+
+        client.receiveMessage(params, function (err: Error, data: AWS.SQS.ReceiveMessageResult) {
+
+            if (data.Messages != null && data.Messages.length > 0) {
+
+                var msg = data.Messages[0];
+                callback(err, msg);
+
+            }
+        });
+
+    }
+
+    deleteMessage(msg: AWS.SQS.Message, callback: (err: Error) => void) {
+        let client = this.sqs;
+        let params: any | AWS.SQS.DeleteMessageRequest = {}; //AWS.SQS.DeleteMessageRequest
+
+        if (msg == null) {
+            callback(new errors.NullOrEmptyArgumentError("msg"));
+            return;
+        }
+
+        if (msg.ReceiptHandle == null || msg.ReceiptHandle.length == 0) {
+            callback(new errors.InvalidArgumentError("msg.ReceiptHandle cannot be null or empty"));
+            return;
+        }
+
+        params.QueueUrl = this.queueName,
+            params.ReceiptHandle = msg.ReceiptHandle;
+
+        client.deleteMessage(params, function (err: Error, data: any) {
+            callback(err);
+        });
+    }
+
+    sendMessage(params: AWS.SQS.SendMessageRequest, callback: (err: Error) => void) {
+        var client = this.sqs;
+
+        if (params.MessageBody == null) {
+            callback(new errors.NullOrEmptyArgumentError("Data cannot be null"));
+            return;
+        }
+        if (params.MessageBody.length > 262144) {
+            callback(new errors.InvalidArgumentError("data too large for SQS"));
+            return;
+        }
+
+        // var params: AWS.SQS.SendMessageRequest = {};
+
+        params.QueueUrl = this.queueName;
+
+        client.sendMessage(params, function (err, data) {
+
+            callback(err);
+        });
+
+    }
 }
--- lib/Interfaces.ts
+++ lib/Interfaces.ts
@@ -3,27 +3,27 @@ import events = require("events");
 
 export interface IQueue {
   queueName: string;
-  getMessage(callback: (err: Error, data: AWS.Sqs.Message) => void);
-  deleteMessage(msg: AWS.Sqs.Message, callback: (err: Error) => void);
-  sendMessage(data: string, callback: (err: Error) => void);
+  getMessage(callback: (err: Error, data: AWS.SQS.Message) => void);
+  deleteMessage(msg: AWS.SQS.Message, callback: (err: Error) => void);
+  sendMessage(params: AWS.SQS.SendMessageRequest, callback: (err: Error) => void);
   createQueueReader(): IQueueReader;
   drain(callback?: (err: Error) => void);
 }
 
 export interface IQueueReader extends events.EventEmitter, IMessageDeleter {
-  onReceipt(callback: (err, messages: AWS.Sqs.Message[], context: IMessageDeleter) => void);
+  onReceipt(callback: (err, messages: AWS.SQS.Message[], context: IMessageDeleter) => void);
   onEmpty(callback: (err) => void);
   start();
   stop();
   pause();
-  receiptCallback: (err: Error, messages: AWS.Sqs.Message[], context: IMessageDeleter) => void;
+  receiptCallback: (err: Error, messages: AWS.SQS.Message[], context: IMessageDeleter) => void;
   emptyCallback: (err: Error) => void;
   errorHandler: (err: Error) => void;
 }
 
 export interface IMessageDeleter {
-  deleteMessage(message: AWS.Sqs.Message);
-  deleteMessages(messages: AWS.Sqs.Message[]);
+  deleteMessage(message: AWS.SQS.Message);
+  deleteMessages(messages: AWS.SQS.Message[]);
   flushReceiptLog();
 }
 
